978c96fc403564511d75260bc5c2c3a55bd88796
Maureen Kariuki <kmaureen44@gmail.com>
2018-10-23 20:54:56 -0700

    Added WebMVC project Added Infrastructure folder in the WebMVC project Added Catalog.cs and CatalogEvent.cs Added Index.cshtml and _layout.cshtml Added wwwroot folder

---
+++ WebMVC/Infrastructure/ApiPaths.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class Basket
+        {
+            public static string GetBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+
+            public static string UpdateBasket(string baseUri)
+            {
+                return baseUri;
+            }
+
+            public static string CleanBasket(string baseUri, string basketId)
+            {
+                return $"{baseUri}/{basketId}";
+            }
+        }
+        public static class Catalog
+        {
+            public static string GetAllCatalogEvents(string baseUri, 
+                int page, int take, int? company, int? type)
+            {
+                var filterQs = string.Empty;
+
+                if (company.HasValue || type.HasValue)
+                {
+                    var companyQs = (company.HasValue) ? company.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/company/{companyQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetCatalogEvent(string baseUri, int id)
+            {
+
+                return $"{baseUri}/events/{id}";
+            }
+            public static string GetAllBrands(string baseUri)
+            {
+                return $"{baseUri}catalogEvents";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogTypes";
+            }
+        }
+        public static class Order
+        {
+            public static string GetOrder(string baseUri, string orderId)
+            {
+                return $"{baseUri}/{orderId}";
+            }
+
+            //public static string GetOrdersByUser(string baseUri, string userName)
+            //{
+            //    return $"{baseUri}/userOrders?userName={userName}";
+            //}
+            public static string GetOrders(string baseUri)
+            {
+                return baseUri;
+            }
+            public static string AddNewOrder(string baseUri)
+            {
+                return $"{baseUri}/new";
+            }
+        }
+
+    }
+}
