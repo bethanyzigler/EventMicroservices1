79c90a4a848f802b6879125fbf53facd64008977
Maureen Kariuki <kmaureen44@gmail.com>
2018-10-27 13:42:44 -0700

    Conflicts resolved

--- WebMVC/Infrastructure/IHttpClient.cs
+++ WebMVC/Infrastructure/IHttpClient.cs
@@ -10,7 +10,7 @@ namespace WebMvc.Infrastructure
     {
         Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
 
-        Task<HttpResponseMessage> PostAsync<T>(string uri, T @event, string authorizationToken = null, string authorizationMethod = "Bearer");
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer");
 
         Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
 
